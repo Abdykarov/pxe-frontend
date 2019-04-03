e81514e753ffec0ee79fac817f1e677aaee28c3c
Zdeněk Staněk <zdenek.stanek@lundegaard.eu>
2019-04-03 15:54:40 +0200

    PXEPR-71 - apollo links

--- src/common/graphql/middleware/apollo-graphql-provider.ts
+++ src/common/graphql/middleware/apollo-graphql-provider.ts
@@ -20,91 +20,93 @@ import {
 } from '../resolvers';
 import { environment } from 'src/environments/environment';
 
-export const ApolloGraphQLProvider = {
-    provide: APOLLO_OPTIONS,
-    useFactory: (httpLink: HttpLink, authService: AuthService, router: Router) => {
-        const cache = new InMemoryCache();
+const apolloGraphQLFactory = (httpLink: HttpLink, authService: AuthService, router: Router) => {
+    const cache = new InMemoryCache();
 
-        const setTokenHeader = (operation: Operation): void => {
-            const token = authService.getToken();
-            if (token) {
-                operation.setContext({
-                    headers: {
-                        authorization: `Bearer ${token}`,
+    const setTokenHeader = (operation: Operation): void => {
+        const token = authService.getToken();
+        if (token) {
+            operation.setContext({
+                headers: {
+                    authorization: `Bearer ${token}`,
+                },
+            });
+        }
+    };
+
+    const http = httpLink.create({
+        uri: `${environment.graphql}/graphql`,
+    });
+
+    const local = withClientState({
+        cache,
+        defaults,
+        resolvers,
+    });
+
+    const auth = new ApolloLink((operation: Operation, forward: NextLink) => {
+        setTokenHeader(operation);
+
+        return new Observable(observer => {
+            let subscription, innerSubscription;
+            try {
+                subscription = forward(operation).subscribe({
+                    next: observer.next.bind(observer),
+                    complete: observer.complete.bind(observer),
+                    error: networkError => {
+                        if (networkError.status === 401) {
+                            authService.refreshToken()
+                                .subscribe(() => {
+                                    setTokenHeader(operation);
+                                    innerSubscription = forward(operation).subscribe(observer);
+                                }, () => {
+                                    observer.error(new Error('jwt refresh failed'));
+                                    router.navigate(['/logout']);
+                                });
+                        } else {
+                            observer.error(networkError);
+                        }
                     },
                 });
+            } catch (e) {
+                observer.error(e);
             }
-        };
-
-        const http = httpLink.create({
-            uri: `${environment.graphql}/graphql`,
-        });
-
-        const local = withClientState({
-            cache,
-            defaults,
-            resolvers,
-        });
-
-        const auth = new ApolloLink((operation: Operation, forward: NextLink) => {
-            setTokenHeader(operation);
-
-            return new Observable(observer => {
-                let subscription, innerSubscription;
-                try {
-                    subscription = forward(operation).subscribe({
-                        next: observer.next.bind(observer),
-                        complete: observer.complete.bind(observer),
-                        error: networkError => {
-                            if (networkError.status === 401) {
-                                authService.refreshToken()
-                                    .subscribe(() => {
-                                        setTokenHeader(operation);
-                                        innerSubscription = forward(operation).subscribe(observer);
-                                    }, () => {
-                                        observer.error(new Error('jwt refresh failed'));
-                                        router.navigate(['/logout']);
-                                    });
-                            } else {
-                                observer.error(networkError);
-                            }
-                        },
-                    });
-                } catch (e) {
-                    observer.error(e);
+            return () => {
+                if (subscription) {
+                    subscription.unsubscribe();
                 }
-                return () => {
-                    if (subscription) {
-                        subscription.unsubscribe();
-                    }
-                    if (innerSubscription) {
-                        innerSubscription.unsubscribe();
-                    }
-                };
-            });
+                if (innerSubscription) {
+                    innerSubscription.unsubscribe();
+                }
+            };
         });
+    });
 
-        const error = onError(({ graphQLErrors, networkError }) => {
-            if (graphQLErrors) {
-                graphQLErrors.map(({ message, locations, path }) =>
-                    console.log('%c ***** [GraphQL error] *****', 'background: red; color: #fff; font-weight: bold',
-                        `Message: ${message}, Location: ${locations}, Path: ${path}`),
-                );
-            }
+    const error = onError(({ graphQLErrors, networkError }) => {
+        if (graphQLErrors) {
+            graphQLErrors.map(({ message, locations, path }) =>
+                console.log('%c ***** [GraphQL error] *****', 'background: red; color: #fff; font-weight: bold',
+                    `Message: ${message}, Location: ${locations}, Path: ${path}`),
+            );
+        }
 
-            if (networkError) {
-                console.log('%c ***** [Network error] *****', 'background: red; color: #fff; font-weight: bold', networkError);
-            }
-        });
+        if (networkError) {
+            console.log('%c ***** [Network error] *****', 'background: red; color: #fff; font-weight: bold', networkError);
+        }
+    });
 
-        // TODO restLink, retryLink?
+    // TODO restLink, retryLink?
 
-        return {
-            cache,
-            link: from([error, local, auth, http]),
-            connectToDevTools: !environment.production,
-        };
-    },
+    return {
+        cache,
+        link: from([error, local, auth, http]),
+        connectToDevTools: !environment.production,
+    };
+};
+
+export const ApolloGraphQLProvider = {
+    provide: APOLLO_OPTIONS,
+    useFactory: apolloGraphQLFactory,
     deps: [
         HttpLink,
         AuthService,
