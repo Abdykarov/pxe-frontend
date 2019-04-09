0de5dab9c849ef346f1ba1a1c61f955e08ddf1ae
Jaroslav Pavelka <jaroslav.pavelka@lundegaard.eu>
2019-04-09 14:43:55 +0200

    PXEPR-55-forms checkbox error state basic styles edit, minor fix

--- src/static/atoms/forms/page.ts
+++ src/static/atoms/forms/page.ts
@@ -12,25 +12,6 @@ export class FormsPageComponent {
     public dropdownItems: Array<IDropdownItem>;
     public breadcrumbItemsSimple: IBreadcrumbItems;
 
-    public options = [
-        {
-            key: 1,
-            value: 'value-one',
-            label: 'Radio button one',
-        },
-        {
-            key: 2,
-            value: 'value-two',
-            label: 'Radio button two',
-        },
-        {
-            key: 3,
-            value: 'value-three',
-            label: 'Disabled radio button',
-            disabled: true, 
-        },
-    ];
-
     constructor(public config: FormsPageConfig,) {
         this.dropdownItems = [];
         const item1 = <IDropdownItem>{
