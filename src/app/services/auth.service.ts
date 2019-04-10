5f49649f218a721553e765929c7118a668e502c5
Zdeněk Staněk <zdenek.stanek@lundegaard.eu>
2019-04-10 11:20:06 +0200

    PXEPR-71 - merge conflicts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { map } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { map} from 'rxjs/operators';
 
 import { CookiesService } from './cookies.service';
 import { environment } from 'src/environments/environment';
