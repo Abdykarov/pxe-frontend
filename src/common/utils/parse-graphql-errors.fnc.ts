ec96597b3234ad4a2c452f9d6c8d164675831586
Zdeněk Staněk <zdenek.stanek@lundegaard.eu>
2019-04-11 10:23:57 +0200

    PXEPR-64 - new subscription component (adding missing types)

--- src/common/utils/parse-graphql-errors.fnc.ts
+++ src/common/utils/parse-graphql-errors.fnc.ts
@@ -1,6 +1,8 @@
 import * as R from 'ramda';
 import * as R_ from 'ramda-extension';
 
+import { ErrorResponse } from 'apollo-link-error';
+
 import { IFieldError } from 'src/common/ui/news-subscription/models/form-definition.model';
 
 const mapValidationFieldArrayToValidationObj = (array) => {
@@ -11,31 +13,35 @@ const mapValidationFieldArrayToValidationObj = (array) => {
     return R.map(prepareKeys)(array);
 };
 
-export const parseGraphQLErrors = (error) => {
-    let fieldError: IFieldError = {};
-    let globalError: string[] = [];
-    const {graphQLErrors, networkError, message} = error;
-    if (!R_.isNilOrEmpty(graphQLErrors)) {
-        const errors = R.head(graphQLErrors);
-        if (errors.validationError) {
-            if (errors.validationError.field) {
-                fieldError = mapValidationFieldArrayToValidationObj(errors.validationError.field);
+export const parseGraphQLErrors = (error: ErrorResponse):
+    {
+        fieldError: IFieldError,
+        globalError: string[],
+    } => {
+        let fieldError: IFieldError = {};
+        let globalError: string[] = [];
+        const {graphQLErrors, networkError} = error;
+        if (!R_.isNilOrEmpty(graphQLErrors)) {
+            const errors = R.head(graphQLErrors);
+            if (errors.validationError) {
+                if (errors.validationError.field) {
+                    fieldError = mapValidationFieldArrayToValidationObj(errors.validationError.field);
+                }
+                if (errors.validationError.global) {
+                    globalError = errors.validationError.global;
+                }
+            } else {
+                globalError.push(errors.message);
             }
-            if (errors.validationError.global) {
-                globalError = errors.validationError.global;
-            }
-        } else {
-            globalError.push(errors.message);
         }
-    }
-    if (networkError) {
-        globalError.push(networkError.message);
-    }
-    if (!networkError && !graphQLErrors) {
-        globalError.push(message);
-    }
-    return {
-        fieldError,
-        globalError,
-    };
+        if (networkError) {
+            globalError.push(networkError.message);
+        }
+        if (!networkError && !graphQLErrors) {
+            globalError.push('Something went wrong!');
+        }
+        return {
+            fieldError,
+            globalError,
+        };
 };
