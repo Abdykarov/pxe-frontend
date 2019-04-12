32e4ac7c63836cf2e1e185719e306960ef8ef49e
Zdeněk Staněk <zdenek.stanek@lundegaard.eu>
2019-04-12 19:06:59 +0200

    PXEPR-100 - footer update

---
+++ src/app/pages/terms-of-use/terms-of-use.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pxe-term-of-use',
+    templateUrl: './terms-of-use.component.html',
+    styleUrls: ['./terms-of-use.component.scss'],
+})
+export class TermsOfUseComponent {}
