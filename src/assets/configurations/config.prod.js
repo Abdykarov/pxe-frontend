91a56e0f269ff298890f335e8da2929f9e6cf66a
Zdeněk Staněk <zdenek.stanek@lundegaard.eu>
2019-04-12 10:06:58 +0200

    config update for current prev env (revert)

--- src/assets/configurations/config.prod.js
+++ src/assets/configurations/config.prod.js
@@ -5,7 +5,7 @@ window.angularDevstack = {
     "config": {
         "gtmId": "UA-137974323-1",
         "production": true,
-        "url": "https://pxeparc-preview.lnd.bz/",
+        "url": "http://localhost:4200",
         "web_api_key": "default-api-key",
         "x_api_key": "6DBE159EAAB67C2AFD31DED689608711"
     }
