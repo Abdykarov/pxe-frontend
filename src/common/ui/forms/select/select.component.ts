4430d814647d5d3f120ba5a757ba4bcfe5acb32e
Martin Kadlec <martin.kadlec@lundegaard.eu>
2019-04-18 16:19:56 +0200

    PXEPR-119 Select UI component

---
+++ src/common/ui/forms/select/select.component.ts
@@ -0,0 +1,48 @@
+import {
+    Component,
+    Input,
+    ViewEncapsulation,
+} from '@angular/core';
+
+import { IOption } from '../models/option.model';
+
+@Component({
+    selector: 'lnd-select',
+    templateUrl: './select.component.html',
+    styleUrls: ['./select.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+})
+export class SelectComponent {
+    @Input()
+    public disabledField = false;
+
+    @Input()
+    public error?: any;
+
+    @Input()
+    public label?: string;
+
+    @Input()
+    public parentForm;
+
+    @Input()
+    public selectName: string;
+
+    @Input()
+    public options?: Array<IOption>;
+
+    @Input()
+    public searchable = false;
+
+    @Input()
+    public subtext?: string;
+
+    @Input()
+    public warning?: any;
+
+    public customSearchFn = (term: string, item: any) => {
+        if (!!term && term.length > 2) {
+            return item.label.toLocaleLowerCase().indexOf(term) > -1 || item.label.indexOf(term) > -1;
+        }
+    }
+}
--- src/static/atoms/forms/page.ts
+++ src/static/atoms/forms/page.ts
@@ -1,42 +1,32 @@
 import { Component } from '@angular/core';
+import {
+    FormControl,
+    FormGroup,
+} from '@angular/forms';
 
-import { IBreadcrumbItems } from 'src/common/ui/breadcrumb/models/breadcrumb.model';
-import { IDropdownItem } from 'src/common/ui/dropdown/models/item.model';
 import { FormsPageConfig } from './config';
+import { IBreadcrumbItems } from 'src/common/ui/breadcrumb/models/breadcrumb.model';
 
 @Component({
-  templateUrl: './page.html',
+    templateUrl: './page.html',
 })
-
 export class FormsPageComponent {
-    public dropdownItems: Array<IDropdownItem>;
+
     public breadcrumbItemsSimple: IBreadcrumbItems;
+
+    public selectBoxes: FormGroup = new FormGroup({
+        simpleSelect: new FormControl(),
+        searchableSelect: new FormControl(),
+    });
 
     constructor(
         public config: FormsPageConfig,
     ) {
-        this.dropdownItems = [];
-        const item1 = <IDropdownItem>{
-                label: 'FOO',
-                action: () => console.log('Click on dropdown item 1'),
-            };
-        const item2 = <IDropdownItem>{
-                label: 'BAR',
-                action: () => console.log('Click on dropdown item 2'),
-            };
-
-        this.dropdownItems.push(item1);
-        this.dropdownItems.push(item2);
-
         this.breadcrumbItemsSimple = [
             {
                 label: 'Forms',
                 url: null,
             },
         ];
-    }
-
-    public clickAction = () => {
-        alert('CLICK');
     }
 }
