1790d6f97089c82a53694d7a4770c8bb602df34b
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2019-05-06 15:18:43 +0200

    feature/PXEPR-166
    formatovani

--- src/common/containers/address-whisperer/services/mapy-cz-api.service.ts
+++ src/common/containers/address-whisperer/services/mapy-cz-api.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
 import {
     HttpClient,
     HttpParams,
 } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
 import * as R from 'ramda';
 import { map } from 'rxjs/operators';
