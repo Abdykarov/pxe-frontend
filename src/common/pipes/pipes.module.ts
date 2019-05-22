ff1ba3be18b0930244520096200b79ab51a9dd09
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2019-05-22 13:17:54 +0200

    feature/PXEPR-178-uprava-vypoctu
    fix

--- src/common/pipes/pipes.module.ts
+++ src/common/pipes/pipes.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 // own classes
 import { ConsumptionPipe } from './consumption/consumption.pipe';
 import { FilterCommodityTypePipe } from './filter-commodity-type/filter-commodity-type.pipe';
-import { ConsumptionIndicatorPipe } from './consumption-indicator-pipe/consumption-indicator.pipe';
+import { ConsumptionIndicatorPipe } from './consumption-indicator/consumption-indicator.pipe';
 import { IsRouteActivePipe } from './is-route-active/is-route-acrive.pipe';
 import { KeysPipe } from './keys/keys.pipe';
 import { PluralPipe } from './plurar/plural.pipe';
