8f7911e3362c0f8d842bd27185e847823d5f3ce3
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2019-05-23 16:47:03 +0200

    feature/PXEPR-220-prehlidka-nabidek-static-to-ui
    formatovani

--- src/common/ui/supply-point-offer/supply-point-offer.module.ts
+++ src/common/ui/supply-point-offer/supply-point-offer.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { ButtonModule } from '../button/button.module';
-import { IndicatorModule } from '../indicator/indicator.module';
+import { ButtonModule } from 'src/common/ui/button/button.module';
+import { IndicatorModule } from 'src/common/ui/indicator/indicator.module';
+import { PipesModule } from 'src/common/pipes/pipes.module';
 import { SupplyPointOfferComponent } from './supply-point-offer.component';
-import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
     declarations: [
