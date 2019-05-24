77173bf6e467da2920c3ae53e2bc77eb480ec8e1
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2019-05-24 16:00:19 +0200

    feature/PXEPR-220-prehlidka-nabidek-static-to-ui
    fix

--- src/common/ui/supply-point-offer/supply-point-offer.component.ts
+++ src/common/ui/supply-point-offer/supply-point-offer.component.ts
@@ -6,12 +6,6 @@ import {
     Output,
 } from '@angular/core';
 
-import {
-    debounceTime,
-    takeUntil,
-} from 'rxjs/operators';
-import { fromEvent } from 'rxjs';
-
 import { AbstractComponent } from 'src/common/abstract.component';
 import { IOffer } from 'src/common/graphql/models/offer.model';
 
@@ -21,7 +15,6 @@ import { IOffer } from 'src/common/graphql/models/offer.model';
     styleUrls: ['./supply-point-offer.component.scss'],
 })
 export class SupplyPointOfferComponent extends AbstractComponent implements OnInit {
-    public allowGlobalClick: boolean;
     public showBenefits = false;
 
     @Input()
@@ -32,25 +25,6 @@ export class SupplyPointOfferComponent extends AbstractComponent implements OnIn
 
     @Output()
     action: EventEmitter<any> = new EventEmitter();
-
-    resizeEvent$ = fromEvent(window, 'resize')
-        .pipe(
-            takeUntil(this.destroy$),
-            debounceTime(200),
-        );
-
-    ngOnInit() {
-        this.setComponentByViewPort();
-
-        this.resizeEvent$.subscribe(() => {
-            this.setComponentByViewPort();
-        });
-    }
-
-    public setComponentByViewPort = () => {
-        this.allowGlobalClick = window.innerWidth > 992;
-        this.showBenefits = this.allowGlobalClick;
-    }
 
     public toggleBenefits(evt) {
         evt.preventDefault();
