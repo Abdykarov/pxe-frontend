2dcf7a7cab70be211bbc53e6f9ab9e79960baba4
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2019-05-30 10:15:58 +0200

    feature/PXEPR-209

--- src/common/pipes/date-diff/date-diff.pipe.ts
+++ src/common/pipes/date-diff/date-diff.pipe.ts
@@ -13,10 +13,6 @@ export class DateDiffPipe implements PipeTransform {
     transform(dateFromString: string, dateToString: string, resultUnit: unitOfTime.Diff = 'days'): number {
         const from = moment(dateFromString);
         const to = moment(dateToString);
-
-        console.log(from);
-        console.log(to);
-
         return to.diff(from, resultUnit);
     }
 }
