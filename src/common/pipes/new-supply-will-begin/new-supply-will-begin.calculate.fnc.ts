4b1170775d962f7061b7f2c68a035afa7514c79b
Zdeněk Staněk <zdenek.stanek@lundegaard.eu>
2019-07-15 18:48:00 +0200

    PXEPR-374 - update-create-supply-points (lint fix)

--- src/common/pipes/new-supply-will-begin/new-supply-will-begin.calculate.fnc.ts
+++ src/common/pipes/new-supply-will-begin/new-supply-will-begin.calculate.fnc.ts
@@ -7,13 +7,13 @@ import { TimeToContractEndPeriod } from 'src/common/graphql/models/supply.model'
 export const getNextDayFromExpirationDate = (form: IFormSupplyPointDefinition) =>
     form.expirationDate && moment(form.expirationDate).add(1, 'days');
 
+export const calculateTerminateInterval = (from: Moment, form: IFormSupplyPointDefinition) =>
+    moment(from).add(form.timeToContractEnd, form.timeToContractEndPeriodId === TimeToContractEndPeriod.DAY ? 'days' : 'month');
+
 export const contractEndTermWithProlongationCalculate = (form: IFormSupplyPointDefinition) =>
     calculateTerminateInterval(moment(form.expirationDate), form).diff(moment()) < 0 && getNextDayFromExpirationDate(form) ;
 
 export const contractEndIndefinitePeriodCalculate = (form: IFormSupplyPointDefinition) =>
     form.timeToContractEnd && form.timeToContractEndPeriodId &&
     calculateTerminateInterval(moment().add(30, 'days'), form).add(1, 'months').startOf('month');
-
-export const calculateTerminateInterval = (from: Moment, form: IFormSupplyPointDefinition) =>
-    moment(from).add(form.timeToContractEnd, form.timeToContractEndPeriodId === TimeToContractEndPeriod.DAY ? 'days' : 'month');
 
