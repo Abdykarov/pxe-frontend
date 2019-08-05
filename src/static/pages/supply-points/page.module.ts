713c82677d5b3f2dfbbe033eb0cbe59472bdf6d8
Jaroslav Pavelka <jaroslav.pavelka@lundegaard.eu>
2019-08-05 17:19:51 +0200

    PXEPR-357 supply point actions add and badge add to staticsupply points

--- src/static/pages/supply-points/page.module.ts
+++ src/static/pages/supply-points/page.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
 
+import { BadgeModule } from 'src/common/ui/badge/badge.module';
 import { BreadcrumbModule } from 'src/common/ui/breadcrumb/breadcrumb.module';
 import { ButtonModule } from 'src/common/ui/button/button.module';
 import { SupplyPointsPageComponent } from './page';
@@ -15,6 +16,7 @@ import { SupplyPointModule } from 'src/common/ui/supply-point/supply-point.modul
         SupplyPointsPageComponent,
     ],
     imports: [
+        BadgeModule,
         BreadcrumbModule,
         ButtonModule,
         CommonModule,
