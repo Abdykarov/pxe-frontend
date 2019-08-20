cce7b3f25718c523d8f9dc4c916fa06d7722cbb4
Zdeněk Staněk <zdenek.stanek@lundegaard.eu>
2019-08-20 15:17:19 +0200

    PXEPR-466 - payty2-codelist

--- src/app/pages/request/recapitulation/recapitulation.module.ts
+++ src/app/pages/request/recapitulation/recapitulation.module.ts
@@ -5,6 +5,7 @@ import { AlertModule } from 'src/common/ui/alert/alert.module';
 import { ButtonModule } from 'src/common/ui/button/button.module';
 import { LayoutContainerModule } from 'src/common/containers/layout-container/layout-container.module';
 import { PersonalInfoFormModule } from 'src/common/containers/form/forms/personal-info/personal-info-form.module';
+import { PlaceloaderModule } from 'src/common/ui/placeloader/placeloader.module';
 import { ProgressBarModule } from 'src/common/ui/progress-bar/progress-bar.module';
 import { RecapitulationComponent } from './recapitulation.component';
 import { RecapitulationRoutingModule } from './recapitulation.routing';
@@ -19,6 +20,7 @@ import { RecapitulationRoutingModule } from './recapitulation.routing';
         CommonModule,
         LayoutContainerModule,
         PersonalInfoFormModule,
+        PlaceloaderModule,
         ProgressBarModule,
         RecapitulationRoutingModule,
     ],
---
+++ src/static/pages/recapitulation/config.ts
@@ -0,0 +1,14 @@
+import { IOption } from 'src/common/ui/forms/models/option.model';
+
+export const depositPaymentType: IOption[] = [
+    {
+        key: 'Inkaso',
+        value: 'Inkaso',
+        label: 'Inkaso',
+    },
+    {
+        key: 'Prikaz',
+        value: 'Prikaz',
+        label: 'Příkaz k úhradě',
+    },
+];
