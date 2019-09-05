29ef26fefd15806e9d534266e9429a2b53cd0de2
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2019-09-05 13:14:58 +0200

    feature/PXEPR-528 -fixes

--- src/app/pages/deleted-account/deleted-account.module.ts
+++ src/app/pages/deleted-account/deleted-account.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ButtonModule } from 'src/common/ui/button/button.module';
 import { DeletedAccountComponent } from './deleted-account.component';
 import { DeletedAccountRoutingModule } from 'src/app/pages/deleted-account/deleted-account.routing';
 import { InfoBannerModule } from 'src/common/ui/info-banner/info-banner.module';
-import { MapCoverageContainerModule } from 'src/common/containers/map-coverage-container/map-coverage-container.module';
 import { LayoutContainerModule } from 'src/common/containers/layout-container/layout-container.module';
-import { RegistrationFormModule } from 'src/common/containers/form/forms/registration/registration-form.module';
 
 @NgModule({
     declarations: [
@@ -17,13 +14,10 @@ import { RegistrationFormModule } from 'src/common/containers/form/forms/registr
         DeletedAccountComponent,
     ],
     imports: [
-        ButtonModule,
         CommonModule,
         InfoBannerModule,
         LayoutContainerModule,
         DeletedAccountRoutingModule,
-        MapCoverageContainerModule,
-        RegistrationFormModule,
     ],
 })
 export class DeletedAccountModule {}
