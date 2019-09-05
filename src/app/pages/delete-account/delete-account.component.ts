bdcefad53cc4548133a6ca5511a9ee2fda95354f
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2019-09-05 14:22:58 +0200

    feature/PXEPR-532 -fixes

--- src/app/pages/delete-account/delete-account.component.ts
+++ src/app/pages/delete-account/delete-account.component.ts
@@ -26,7 +26,7 @@ import { ROUTES } from 'src/app/app.constants';
     styleUrls: ['./delete-account.component.scss'],
 })
 export class DeleteAccountComponent extends AbstractComponent implements OnInit {
-    private readonly MAX_SUPPLY_POINTS_OF_RESULT_IN_CANT_DELETE_TEMPLATE = 5;
+    public readonly MAX_SUPPLY_POINTS_OF_RESULT_IN_CANT_DELETE_TEMPLATE = 5;
 
     public currentUser: IJwtPayload;
     public globalError: string[] = [];
@@ -59,8 +59,7 @@ export class DeleteAccountComponent extends AbstractComponent implements OnInit 
                     R.pipe(
                         this.sortByNameCaseInsensitive,
                         R.uniqBy(R.prop('name')),
-                    )([{name: 'ahoj'}, {name: 'CCc'}, {name: 'ahoj'}, {name: 'ahoj1'},
-                        {name: 'ahoj1'}, {name: 'aaa'}, {name: 'Bbb'}, {name: 'bqweqwebb'}, ])),
+                    )(supplyPoints)),
             )
             .subscribe(
                 (supplyPoints: any) => {
