528628ff803a66ef6a3701d0f9fb58a080f3ca2b
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2019-09-27 13:48:04 +0200

    hotfix/PXEPR-637-new-error-message-current-password

--- src/common/constants/errors.constant.ts
+++ src/common/constants/errors.constant.ts
@@ -191,6 +191,7 @@ export const errorFieldMessages =  {
         pattern: '	Vaše heslo musí mít minimálně 8 písmen, musí obsahovat malá a velká ' +
             'písmena, aspoň jednu číslici a aspoň jeden speciální znak jako &_*+/#\ apod.',
         fieldsMustMatch: 'Musíte vyplnit stejná hesla.',
+        invalidCurrentPassword: 'Současné heslo není shodné.',
     },
     phone: {
         required: 'Vyplňte svůj telefon.',
