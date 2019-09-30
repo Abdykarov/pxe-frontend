0631ccac2de0f5d7b40f846ac73652b7d21ee473
Jaroslav Pavelka <jaroslav.pavelka@lundegaard.eu>
2019-09-30 15:46:36 +0200

    PXEPR-657 static minor fix

--- src/static/pages/error-503/page.module.ts
+++ src/static/pages/error-503/page.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
 
 // own classes
-import { BreadcrumbModule } from 'src/common/ui/breadcrumb/breadcrumb.module';
 import { Error503PageComponent } from './page';
 import { InfoBannerModule } from 'src/common/ui/info-banner/info-banner.module';
 
@@ -16,7 +15,6 @@ import { InfoBannerModule } from 'src/common/ui/info-banner/info-banner.module';
     ],
     imports: [
         CommonModule,
-        BreadcrumbModule,
         InfoBannerModule,
     ],
 })
