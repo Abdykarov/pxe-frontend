59dd51eed61a65bc0b69f3c335a33b5f22024b68
Jaroslav Pavelka <jaroslav.pavelka@lundegaard.eu>
2019-10-03 14:29:42 +0200

    PXEPR-657 error 500 page edit

--- src/static/pages/error-500/page.ts
+++ src/static/pages/error-500/page.ts
@@ -4,6 +4,7 @@ import { BannerTypeImages } from 'src/common/ui/info-banner/models/info-banner.m
 
 @Component({
     templateUrl: './page.html',
+    styleUrls: ['../../../assets/scss/05_pages/error-page.scss'],
 })
 
 export class Error500PageComponent {
