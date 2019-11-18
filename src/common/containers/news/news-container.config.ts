dcd6dff1339432073c74c6a542dfe94f2d5aa5ba
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2019-11-18 13:39:58 +0100

    feature/PXEPR-737-change-new

--- src/common/containers/news/news-container.config.ts
+++ src/common/containers/news/news-container.config.ts
@@ -3,6 +3,6 @@ import { INews } from 'src/common/ui/news/model/news.model';
 export const config: INews = [
     {
         date: new Date('2019-11-20'),
-        text: 'Spustili jsme pro Vás pilotní provoz aplikace parc4u.cz, která Vám pomůže zajistit toho správného dodavatele.',
+        text: 'Spustili jsme pro vás pilotní provoz aplikace parc4u.cz, která vám pomůže zajistit toho správného dodavatele.',
     },
 ];
