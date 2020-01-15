9d75337306f7281602e1b66fc14211865bb67748
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-01-15 09:03:47 +0100

    feature/PXEPR-828

--- src/app/services/s-analytics.service.ts
+++ src/app/services/s-analytics.service.ts
@@ -20,17 +20,42 @@ export class SAnalyticsService {
     public init = () => {
         sa('create', environment.sAnalyticsTId);
         sa('send', 'pageview');
-        sa('send', 'register', '8794FFFFF');
 
-        const applicationWebData = {
-            'applicationId': '8794FFFFF',
-            'loanInfo': {
-                'type': 'string',
-                'amount': 0,
-                'monthlyPayment': 0,
-                'numberOfInstallments': 0,
-            },
-        };
-        sa('send', 'webdata', applicationWebData);
+        // sa('send', 'register', '8794FFFFF');
+        //
+        // const applicationWebData = {
+        //     'applicationId': '8794FFFFF',
+        //     'loanInfo': {
+        //         'type': 'string',
+        //         'amount': 0,
+        //         'monthlyPayment': 0,
+        //         'numberOfInstallments': 0,
+        //     },
+        // };
+        // sa('send', 'webdata', applicationWebData);
+    }
+
+    public installPlugin = (pluginName) => {
+        sa('include', pluginName);
+
+        // const node = this.document.createElement('script');
+        // node.src = dynamicScripts[i];
+        // node.type = 'text/javascript';
+        // node.async = false;
+        // this.document.getElementsByTagName('head')[0].appendChild(node);
+        //
+
+        // System.import(`https://sa-sdp.lnd.bz/versions/stable/${pluginName}.plugin.min.js`);
+
+
+        // (function(d, f, g, e, a, c, b)
+        // {if (c = d.getElementById(g))
+        // {for (b = 0; b < e.length; b++) {a = d.createElement(f),
+        // a.async = 1, a.src = e[b], c.parentNode.insertBefore(a, c.nextSibling); } }
+        // })(document, 'script', 'SAnalyticsScript', [
+        //     'https://sa-sdp.lnd.bz/versions/stable/<pluginName1>.plugin.min.js',
+        //     'https://sa-sdp.lnd.bz/versions/stable/<pluginName2>.plugin.min.js',
+        // ]);
+        // </script>;
     }
 }
