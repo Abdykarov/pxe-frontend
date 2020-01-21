e310a0db070e38e077f2a07c7079cf08bb43c1eb
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-01-21 09:41:19 +0100

    feature/PXEPR-828
    formating

--- src/common/directives/s-analytics/s-form.directive.ts
+++ src/common/directives/s-analytics/s-form.directive.ts
@@ -9,11 +9,14 @@ import { SAnalyticsService } from 'src/app/services/s-analytics.service';
 })
 export class SFormDirective {
 
-    @HostListener('change', ['$event']) change = (event: any) => this.sAnalyticsService.sFormChange(event);
+    @HostListener('change', ['$event']) change = (event: any) =>
+        this.sAnalyticsService.sFormChange(event)
 
-    @HostListener('focus', ['$event']) focus = (event: any) => this.sAnalyticsService.sFormFocus(event);
+    @HostListener('focus', ['$event']) focus = (event: any) =>
+        this.sAnalyticsService.sFormFocus(event)
 
-    @HostListener('blur', ['$event']) blur = (event: any) => this.sAnalyticsService.sFormBlur(event);
+    @HostListener('blur', ['$event']) blur = (event: any) =>
+        this.sAnalyticsService.sFormBlur(event)
 
     constructor(
         private sAnalyticsService: SAnalyticsService,
