073be028f5434601f24cef9443108a8b5e8e5087
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-01-24 11:31:53 +0100

    feature/PXEPR-848

--- src/common/utils/validators/total-digit-length-before-decimal-point-validator.fnc.ts
+++ src/common/utils/validators/total-digit-length-before-decimal-point-validator.fnc.ts
@@ -2,7 +2,10 @@ import * as R from 'ramda';
 
 const getLengthOfNumber = (number: number): number => number.toString().length;
 
-export const totalDigitLengthBeforeDecimalPointValidator = (number: string, length: number): boolean => R.pipe(
+export const totalDigitLengthBeforeDecimalPointValidator = (number: string | number, length: number): boolean => R.pipe(
+    (num: string | number): string => {
+        return '' + num;
+    },
     R.replace('.', ','),
     parseFloat,
     getLengthOfNumber,
