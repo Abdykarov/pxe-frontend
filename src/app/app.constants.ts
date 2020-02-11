598822fc070bd131b56f1d791cfaea327d9cc2b4
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-02-11 07:37:44 +0100

    feature/PXEPR-852
    add keywords pxe a parc4u

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -155,6 +155,8 @@ export const SEO = {
             'nejlevnějši plyn',
             'distribuce plynu',
             'distribuce elektřiny',
+            'parc4u',
+            'pxe',
         ],
         LOGIN: [
             'přihlášení',
