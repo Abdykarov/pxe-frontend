9c475fa06cb9e6f72c6b16eac46bddb0091bbc74
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-03-09 12:51:46 +0100

    feature/PXEPR-905-refresh-token-duration-90-mins
    -change duration to 90 min from 30

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -56,7 +56,7 @@ export const CONSTS = {
     },
     REFRESH_TOKEN: {
         INTERVAL: 300000,
-        COUNT: 4,
+        COUNT: 16,
         DONT_REFRESH_TIME_IN_MINUTES: 2,
     },
     VALIDATORS: {
