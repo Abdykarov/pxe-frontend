00a556f6363ad3ee361d028b908513b8c07c9ee1
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-03-09 10:53:14 +0100

    feature/PXEPR-863-approv-import

--- src/common/utils/transform-http-headers-to-file-uploader-format.fnc.ts
+++ src/common/utils/transform-http-headers-to-file-uploader-format.fnc.ts
@@ -5,10 +5,8 @@ import * as R from 'ramda';
 import { Headers } from 'src/third-sides/file-upload';
 
 export const transformHttpHeadersToFileUploaderFormat = (httpHeaders: HttpHeaders): Headers[] => R.map(
-        (key: string) => {
-            return {
-                name: key,
-                value: httpHeaders.get(key),
-            };
-        },
+        (name: string): Headers => ({
+            name,
+            value: httpHeaders.get(name),
+        }),
     )(httpHeaders.keys());
