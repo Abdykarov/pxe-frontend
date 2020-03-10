8a7e67f034b529fa88453d179af257f51fea195b
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-03-10 09:41:41 +0100

    feature/PXEPR-871-update-landing-page

---
+++ src/common/ui/carousel/carousel.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+
+import { ICarouselItem } from 'src/common/ui/carousel/models/data.model';
+
+@Component({
+    selector: 'lnd-carousel',
+    templateUrl: './carousel.component.html',
+    styleUrls: ['./carousel.component.scss'],
+})
+export class CarouselComponent {
+
+    @Input()
+    public interval: boolean | number = false;
+
+    @Input()
+    public carouselItems: ICarouselItem[];
+
+    @Output()
+    public activeSlideChange: EventEmitter<any> = new EventEmitter<any>();
+
+}
