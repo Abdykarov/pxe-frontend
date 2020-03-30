c8dfcb843d61e1256df2024e0d541337a62c6de4
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-03-30 11:18:14 +0200

    feature/PXEPR-934-bad-refresh

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -221,7 +221,8 @@ export class AuthService {
             if (window.sessionStorage) {
                 window.sessionStorage.setItem('uuid', uuid);
             }
-            this.cookiesService.setObject(this.cookieName, user, new Date().getTime() + (CONSTS.DEFAULT_EXPIRATION * 1000));
+            const expiration = new Date().getTime() + CONSTS.DEFAULT_EXPIRATION * 1000;
+            this.cookiesService.setObject(this.cookieName, user, expiration);
             this.checkLogin();
             this.currentUserSubject$.next(jwtPayload);
         }
