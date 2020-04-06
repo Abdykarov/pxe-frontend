ae28e5e08e84d2e74e9b461cadd58d10834821e2
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-04-06 20:10:09 +0200

    feature/PXEPR-871-update-landing-page
    add to app

--- src/common/ui/carousel/carousel.component.ts
+++ src/common/ui/carousel/carousel.component.ts
@@ -1,4 +1,9 @@
-import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    Output,
+} from '@angular/core';
 
 import { ICarouselItem } from 'src/common/ui/carousel/models/data.model';
 
