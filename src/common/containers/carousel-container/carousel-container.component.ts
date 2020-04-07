44fc18bcb8e858870995d86d08f23960bbcee02a
Jaroslav Pavelka <jaroslav.pavelka@lundegaard.eu>
2020-04-07 19:07:09 +0200

    PXEPR-871-update-landing-page minor fix

--- src/common/containers/carousel-container/carousel-container.component.ts
+++ src/common/containers/carousel-container/carousel-container.component.ts
@@ -12,22 +12,22 @@ export class CarouselContainerComponent extends AbstractComponent {
         {
             title: 'Nejdříve se seznámíme',
             label: 'Představíte sebe i vaše odběrné místo nebo nahrajete fakturu, ať máme potřebné informace.',
-            alt: 'carousel obrázel - drag and drop funkce',
+            alt: 'carousel obrázek - drag and drop funkce',
             src: '/assets/images/landing-page/laptop-dragdrop.png',
         },
         {
             title: 'Dostanete nabídky',
             label: 'Získáme pro vás ty nejzajímavější nabídky od dodavatelů, které před tím důkladně prověříme, ať se nemusíte ničeho bát.',
-            alt: 'carousel obrázel - zajímavé nabídky',
+            alt: 'carousel obrázek - zajímavé nabídky',
             src: '/assets/images/landing-page/laptop-offers.png',
         },
         {
             title: 'Vyberete si a je to',
             label: 'Nejlákavější nabídku potvrdíte jedním kliknutím a potřebný převod už za vás zařídíme my.',
-            alt: 'carousel obrázel - skvělý přehled',
+            alt: 'carousel obrázek - skvělý přehled',
             src: '/assets/images/landing-page/laptop-overview.png',
         },
     ];
 
-    public readonly  interval = 100;
+    public readonly interval = 0;
 }
