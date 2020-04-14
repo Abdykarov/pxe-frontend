99e4c53c4bf734ed2e6c89c1f3edab1bdde2e711
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-04-14 11:28:20 +0200

    feature/PXEPR-957-consumption-is-not-filled
    fix pipe

--- src/common/pipes/consumption-indicator/consumption-indicator.pipe.ts
+++ src/common/pipes/consumption-indicator/consumption-indicator.pipe.ts
@@ -9,10 +9,10 @@ import * as R_ from 'ramda-extension';
 import { ISupplyPoint } from 'src/common/graphql/models/supply.model';
 
 @Pipe({
-  name: 'consumptionIndicator',
+    name: 'consumptionIndicator',
 })
 export class ConsumptionIndicatorPipe implements PipeTransform {
-    private allNotNil = () => R.all(R_.isNotNil);
+    private allIsTruthy = (values) => R.all(R_.isTruthy)(values);
 
     private count = (last: number, avg: number) => {
         return last / (avg / 100) - 100;
@@ -26,7 +26,7 @@ export class ConsumptionIndicatorPipe implements PipeTransform {
             lastAnnualConsumptionNT = null,
         } = data;
 
-        if (this.allNotNil()([
+        if (this.allIsTruthy([
             annualConsumptionVT,
             annualConsumptionNT,
             lastAnnualConsumptionVT,
@@ -37,11 +37,11 @@ export class ConsumptionIndicatorPipe implements PipeTransform {
             return this.count(annualConsumption, lastAnnualConsumption);
         }
 
-        if (annualConsumptionVT && lastAnnualConsumptionVT) {
+        if (annualConsumptionVT && lastAnnualConsumptionVT && annualConsumptionVT !== lastAnnualConsumptionVT) {
             return this.count(annualConsumptionVT, lastAnnualConsumptionVT);
         }
 
-        if (annualConsumptionNT && lastAnnualConsumptionNT) {
+        if (annualConsumptionNT && lastAnnualConsumptionNT && annualConsumptionNT !== lastAnnualConsumptionNT) {
             return this.count(annualConsumptionNT, lastAnnualConsumptionNT);
         }
 
