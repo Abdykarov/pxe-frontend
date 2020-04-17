653f07c4df1ee94ec5a05d5a93d2c12c6ac44c2a
Jaroslav Pavelka <jaroslav.pavelka@lundegaard.eu>
2020-04-17 12:15:06 +0200

    PXEPR-871-update-landing-page new picture add and minor fix

--- src/common/containers/carousel-container/carousel-container.component.ts
+++ src/common/containers/carousel-container/carousel-container.component.ts
@@ -28,20 +28,20 @@ export class CarouselContainerComponent extends AbstractComponent {
         {
             title: 'Nejdříve se seznámíme',
             label: 'Představíte nám vaše odběrné místo a základní informace o vaší aktuální smlouvě s dodavatelem.',
-            alt: 'carousel obrázek - drag and drop funkce',
-            src: '/assets/images/landing-page/laptop-dragdrop.png',
+            alt: 'carousel obrázek - Nejdříve se seznámíme',
+            src: '/assets/images/landing-page/laptop-supply-point.png',
         },
         {
             title: 'Dostanete nabídky',
             label: 'Získáme pro vás ty nejzajímavější nabídky od dodavatelů, které před tím důkladně prověříme, ať se nemusíte ničeho bát.',
-            alt: 'carousel obrázek - zajímavé nabídky',
+            alt: 'carousel obrázek - Dostanete nabídky',
             src: '/assets/images/landing-page/laptop-offers.png',
         },
         {
             title: 'Vyberete si a podepíšete smlouvu',
             label: 'Nejlákavější nabídku potvrdíte jedním kliknutím, doplníte nám základní informace o majiteli odběrného místa a smlouvu online podepíšete.',
-            alt: 'carousel obrázek - skvělý přehled',
-            src: '/assets/images/landing-page/laptop-overview.png',
+            alt: 'carousel obrázek - Vyberete si a podepíšete smlouvu',
+            src: '/assets/images/landing-page/laptop-payment.png',
         },
     ];
 
