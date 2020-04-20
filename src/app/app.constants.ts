93a455ec2a2a4e0d932ef6d139a74754054bd4c5
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-04-20 14:10:19 +0200

    feature/PXEPR-914-change-public-menu-for-logged-user

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -88,7 +88,7 @@ export const CONSTS = {
         WILD_CART  : '**',
     },
     REFRESH_TOKEN: {
-        INTERVAL: 1200,
+        INTERVAL: 300000,
         COUNT: 16,
         DONT_REFRESH_TIME_IN_MINUTES: 2,
     },
