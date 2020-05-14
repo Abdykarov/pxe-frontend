b0073a69f3d8251e36b4da7a6f52ecf3fafb7977
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-05-14 21:12:35 +0200

    feature/PXEPR-897

--- src/common/ui/tooltip/tooltip.component.ts
+++ src/common/ui/tooltip/tooltip.component.ts
@@ -1,14 +1,17 @@
 import {
-    Component,
-    Input,
+    Component, ElementRef,
+    Input, Renderer2,
     TemplateRef,
 } from '@angular/core';
 import * as R from 'ramda';
-import { debounceTime } from 'rxjs/operators';
+import { count, debounceTime } from 'rxjs/operators';
 import { fromEvent } from 'rxjs';
 
 // Own models
 import { ITooltipDirection } from './models/direction.model';
+
+const TOOLTIP_WRAPPER = '.tooltip .tooltip__content';
+const PAGE_PADDING = 30;
 
 @Component({
     selector: 'lnd-tooltip',
@@ -25,6 +28,9 @@ export class TooltipComponent {
     @Input()
     public direction?: ITooltipDirection;
 
+    @Input()
+    public countPositionBy = null;
+
     public isOpen: boolean;
 
     private mq = window.matchMedia('(max-width: 992px)');
@@ -34,10 +40,14 @@ export class TooltipComponent {
             debounceTime(200),
         );
 
-    constructor() {
+    constructor(
+        private hostElement: ElementRef,
+        private renderer: Renderer2,
+    ) {
         this.direction = R.contains(this.direction, Object.values(ITooltipDirection)) ? this.direction : ITooltipDirection.LEFT;
         this.allowClick = this.mq.matches;
         this.resizeEvent$.subscribe(() => {
+            this.manageDropdownPosition();
             this.isOpen = false;
             this.allowClick = this.mq.matches;
         });
@@ -47,5 +57,71 @@ export class TooltipComponent {
         if (this.allowClick) {
             this.isOpen = !this.isOpen;
         }
+    }
+
+    public manageDropdownPosition() {
+        const wrapper = this.countPositionBy;
+        const tooltipContent = wrapper.querySelector(TOOLTIP_WRAPPER);
+        const wrapperRect = wrapper.getBoundingClientRect();
+        const tooltipContentRect = tooltipContent.getBoundingClientRect();
+        let left = 0;
+        if (wrapperRect.left - tooltipContentRect.left) {
+            console.log(wrapperRect.left);
+            console.log(tooltipContentRect.left);
+            left = wrapperRect.left - tooltipContentRect.left;
+            console.log(left);
+            // this.renderer.setStyle(tooltipContent, 'left', wrapperRect.left + 'px');
+        }
+        // tooltipContent.
+        // console.log(tooltipContentRect);
+        // console.log(wrapperRect);
+        // console.log(wrapperRect.left);
+        // console.log(tooltipContentRect.left);
+        // console.log(wrapperRect.left - tooltipContentRect.left);
+
+
+        // const clientWidth = document.body.clientWidth;
+        // const clientHeight = document.body.clientHeight;
+        // const banan = this.countPositionBy.nativeElement;
+        // const wrapper = this.hostElement.nativeElement;
+        // const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        // const troll = wrapper.querySelector(TOOLTIP_WRAPPER);
+
+//         const dropdownMenu = wrapper.querySelector(TOOLTIP_WRAPPER);
+//         console.log('_Asdasdads_');
+//         console.log(dropdownMenu);
+//         let left;
+// console.log(wrapper);
+        // wrapper.className = '';
+        // troll.className = '';
+        // this.renderer.removeStyle(dropdownMenu, 'right');
+        // this.renderer.removeStyle(dropdownMenu, 'left');
+
+        // const wrapperRect = wrapper.getBoundingClientRect();
+        // let dropdownMenuRect = dropdownMenu.getBoundingClientRect();
+        //
+        // const isDownAvailable = scrollTop + dropdownMenuRect.height + wrapperRect.bottom + PAGE_PADDING < clientHeight;
+        // const isUpAvailable = dropdownMenuRect.height + 2 * PAGE_PADDING < wrapperRect.bottom;
+        //
+        // if (dropdownMenuRect.width + PAGE_PADDING === clientWidth) {
+        //     return;
+        // }
+
+
+        // left = (wrapperRect.width - dropdownMenuRect.width) / 2;
+        // this.renderer.setStyle(dropdownMenu, 'left', left + 'px');
+        // dropdownMenuRect = dropdownMenu.getBoundingClientRect();
+
+        // console.log(dropdownMenuRect.left);
+        // console.log(left);
+        // console.log('___');
+        //
+        // if (dropdownMenuRect.left < 0) {
+        //     this.renderer.setStyle(dropdownMenu, 'left', 0 + 'px');
+        // }
+        // if (dropdownMenuRect.right > clientWidth - PAGE_PADDING) {
+        //     left = -1 * (dropdownMenuRect.width - wrapperRect.width);
+        //     this.renderer.setStyle(dropdownMenu, 'left', left + 'px');
+        // }
     }
 }
