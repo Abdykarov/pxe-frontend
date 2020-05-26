032ae9cfdee3685e49f1412e238aab18d7c2f42a
Zdeněk Staněk <zdenek.stanek@lundegaard.eu>
2020-05-26 17:12:53 +0200

    PXEPR-897-supply-offer-tooltip - static update

--- src/app/services/model/faq.model.ts
+++ src/app/services/model/faq.model.ts
@@ -7,7 +7,7 @@ export interface IQuestion {
     fullContent?: string;
     oneOfMostVisited?: boolean;
     seoKeywords?: string;
-    vatNumber?: number;
+    vatNumber?: string;
     isTestData?: boolean;
 }
 
