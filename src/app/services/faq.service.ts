b1e28b2c624fa89694abc290908b3b22d8735209
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-05-28 17:20:44 +0200

    feature/PXEPR-440-ssr-test-fix

--- src/app/services/faq.service.ts
+++ src/app/services/faq.service.ts
@@ -33,7 +33,7 @@ export class FaqService {
             .pipe(
                 switchMap((faqConfig: ITagConfigItem[]) => {
                     this.faqConfig = faqConfig;
-                    return http.get(`${environment.url}/'assets/static-data/questions.json`);
+                    return http.get(`${environment.url}/assets/static-data/questions.json`);
                 }),
                 map((questions: IQuestion[]) => {
                     questions = R.map( (question: IQuestion) => {
