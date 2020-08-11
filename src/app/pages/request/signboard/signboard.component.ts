f3ccfba09c3a85f4a71eb2852dec2e3a421dc8e6
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-08-11 15:07:54 +0200

    feature/PXEBER-1046-add-movie-before-create-supply-point

--- src/app/pages/request/signboard/signboard.component.ts
+++ src/app/pages/request/signboard/signboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import { Router } from '@angular/router';
 
 import * as R from 'ramda';
@@ -14,7 +14,12 @@ import { ROUTES } from 'src/app/app.constants';
     styleUrls: ['./signboard.component.scss'],
 })
 export class SignboardComponent {
+
+    @ViewChild('video')
+    public video: ElementRef;
+
     public readonly ACTUAL_PROGRESS_STATUS = ProgressStatus.SUPPLY_POINT;
+    public isVideoPlaying = false;
     public stepperProgressConfig: IStepperProgressItem[] = getConfigStepper(this.ACTUAL_PROGRESS_STATUS);
     public showWelcome = false;
 
@@ -27,5 +32,30 @@ export class SignboardComponent {
     public routerToNextStep = (evt) => {
         evt.preventDefault();
         this.router.navigate([ROUTES.ROUTER_REQUEST_SUPPLY_POINT]);
+    }
+
+    public toggleVideo = (event = null) => {
+        if (event) {
+            event.preventDefault();
+        }
+
+        const video: HTMLMediaElement = this.video.nativeElement;
+        if (!this.isVideoPlaying) {
+            const playPromise = video && video.play();
+            if (!R.isNil(playPromise)) {
+                this.isVideoPlaying = true;
+                playPromise
+                    .then(_ => ({}))
+                    .catch(error => {
+                        video.muted = true;
+                        video.play();
+                    });
+            } else {
+                this.isVideoPlaying = true;
+            }
+        } else {
+            video.pause();
+            this.isVideoPlaying = false;
+        }
     }
 }
