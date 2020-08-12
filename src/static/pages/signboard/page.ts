e18ac0dde93e6efafe42b3678cf06fa57d31454b
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-08-12 13:01:20 +0200

    feature/PXEBER-1046
    add to static

--- src/static/pages/signboard/page.ts
+++ src/static/pages/signboard/page.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    ElementRef,
+    ViewChild,
+} from '@angular/core';
+
+import * as R from 'ramda';
 
 import { NewSupplyPointPageConfig } from 'src/static/pages/new-supply-point/config';
 
@@ -7,6 +13,12 @@ import { NewSupplyPointPageConfig } from 'src/static/pages/new-supply-point/conf
 })
 
 export class SignboardComponent {
+
+    @ViewChild('video')
+    public video: ElementRef;
+
+    public isVideoPlaying = false;
+
     constructor(
         public config: NewSupplyPointPageConfig,
     ) {}
@@ -15,4 +27,30 @@ export class SignboardComponent {
         evt.preventDefault();
         console.log('clicked');
     }
+
+    public toggleVideo = (event = null) => {
+        if (event) {
+            event.preventDefault();
+        }
+
+        const video: HTMLMediaElement = this.video.nativeElement;
+        if (!this.isVideoPlaying) {
+            const playPromise = video && video.play();
+            if (!R.isNil(playPromise)) {
+                this.isVideoPlaying = true;
+                playPromise
+                    .then(_ => ({}))
+                    .catch(error => {
+                        video.muted = true;
+                        video.play();
+                    });
+            } else {
+                this.isVideoPlaying = true;
+            }
+        } else {
+            video.pause();
+            this.isVideoPlaying = false;
+        }
+    }
+
 }
