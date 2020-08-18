e381cea09014deee6e47b0261ad2c85cee1c51de
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-08-18 10:45:02 +0200

    feature/PXEPR-1035-remove-request
    lint fix

--- src/common/utils/append-string-to-spec-position.fnc.ts
+++ src/common/utils/append-string-to-spec-position.fnc.ts
@@ -1,2 +1,2 @@
 export const appendStringToSpecPosition = (str: string, index: number, value: string) =>
-    str.substr(0, index) + value + str.substr(index)
+    str.substr(0, index) + value + str.substr(index);
