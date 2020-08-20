fafbb945933192fca38dbddcf06ae968b1f0e3d7
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-08-20 12:21:54 +0200

    feature/PXEBER-116-address-whisper-autocomplete-by-self-without-error-message

--- src/common/utils/validators/ico-validator.fnc.ts
+++ src/common/utils/validators/ico-validator.fnc.ts
@@ -1,26 +1,57 @@
-export const verifyIC = (icArray: number[]): boolean => {
-    if (icArray.length === 0) {
-        return null;
+import {
+    always,
+    split,
+    isEmpty,
+    map,
+    compose,
+    reduce,
+    dropLast,
+    equals,
+    T,
+    cond,
+    o,
+    modulo,
+    prop,
+    __,
+    subtract,
+} from 'ramda';
+
+const COUNT_NUMBERS_OF_ICO = 8;
+const CONTROL_INDEX = 7;
+
+const countWeightedSum = icArray =>
+    compose(
+        prop('weightedSum'),
+        reduce(
+            ({ weightedSum, index }, number) => ({
+                weightedSum: weightedSum + number * (COUNT_NUMBERS_OF_ICO - index),
+                index: ++index,
+            }),
+            {
+                weightedSum: 0,
+                index: 0,
+            },
+        ),
+        dropLast(1),
+    )(icArray);
+
+const getCompareParameter = cond([
+    [equals(0), always(1)],
+    [equals(1), always(0)],
+    [T, subtract(11)],
+]);
+
+export const verifyIC = ico => {
+    const icArray = o(map(parseInt), split(''))(ico);
+
+    if (isEmpty(icArray)) {
+        return false;
     }
 
-    let sum = 0;
-
-    for (let i = 0; i < 7; i++) {
-        sum += icArray[i] * (8 - i);
-    }
-
-    sum = sum % 11;
-
-    let c: number;
-
-    if (sum === 0) {
-        c = 1;
-    } else if (sum === 1) {
-        c = 0;
-    } else {
-        c = 11 - sum;
-    }
-
-    return icArray[7] === c;
+    return compose(
+        equals(icArray[CONTROL_INDEX]),
+        getCompareParameter,
+        modulo(__, 11),
+        countWeightedSum,
+    )(icArray);
 };
-
