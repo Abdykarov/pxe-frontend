2deeec894b71254ec6ceff5f994dd2bdb9194614
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2020-11-11 17:40:11 +0100

    fix api

--- src/common/graphql/services/contract.service.ts
+++ src/common/graphql/services/contract.service.ts
@@ -11,7 +11,6 @@ import {
 import {
     concludeContractMutation,
     confirmFirstContractViewMutation,
-    deleteContractMutation,
     deleteSelectedOfferFromContractMutation,
     deleteSignedContractMutation,
     saveContractMutation,
