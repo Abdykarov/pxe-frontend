5b13e671c381fe8d0246bf4a28493c9df6a775f1
Jaroslav Pavelka <jaroslav.pavelka@lundegaard.eu>
2020-12-14 11:42:42 +0100

    PXEPR-1205-card styles edit

--- src/static/atoms/card/config.ts
+++ src/static/atoms/card/config.ts
@@ -1,28 +1,3 @@
-export const defaultFirstCard = {
-    imgSrc: 'assets/images/landing-page/our-help-cottage.png',
-    imgAlt: 'Card image',
-    imgTitle: 'Card title',
-    title: 'Card with image and button',
-    footerContent: '<strong>Lorem ipsum dolor sit amet</strong> consectetuer adipiscing elit.',
-};
-
-export const defaultSecondCard = {
-    imgSrc: 'assets/images/landing-page/our-help-cottage.png',
-    imgAlt: 'Card image',
-    imgTitle: 'Card title',
-    title: 'Card with image bottom',
-    content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.' +
-    'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
-};
-
-export const defaultListCard = {
-    imgSrc: 'assets/images/landing-page/our-help-cottage.png',
-    imgAlt: 'Card image',
-    imgTitle: 'Card title',
-    title: 'List card',
-    content: '<strong>Lorem ipsum dolor sit amet</strong> consectetuer adipiscing elit.',
-};
-
 export const defaultTileCard = {
     imgSrc: 'assets/images/landing-page/our-help-cottage.png',
     imgAlt: 'Card image',
