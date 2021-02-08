48164f35fe2c1a621e0ce74fac78b9689dbb6d88
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2021-02-08 10:40:27 +0100

    feature/fix-videos-1252-tempolary

--- src/common/directives/display-none-on-firefox/display-none-on-firefox.directive.ts
+++ src/common/directives/display-none-on-firefox/display-none-on-firefox.directive.ts
@@ -12,6 +12,7 @@ export class DisplayNoneOnFirefoxDirective {
     constructor(
         private el: ElementRef,
     ) {
+
         if (navigator.userAgent.indexOf('Firefox') !== -1) {
             this.el.nativeElement.style.display = 'none';
         }
