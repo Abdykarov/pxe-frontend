a0cc7d5cf7cad1e2b271194e792727c6925b5b60
Jaroslav Pavelka <jaroslav.pavelka@lundegaard.eu>
2021-03-21 19:57:35 +0100

    PXEPR-1274-blog card tyles add, card styles and structure edit

--- src/common/ui/card/card.component.ts
+++ src/common/ui/card/card.component.ts
@@ -4,10 +4,12 @@ import {
     Input,
     Output,
 } from '@angular/core';
+import * as R from 'ramda';
 import { DomSanitizer } from '@angular/platform-browser';
 
 // own models
 import { ICardData } from './models/data.model';
+import { ICardType } from './models/type.model';
 
 @Component({
     selector: 'lnd-card',
@@ -19,8 +21,13 @@ export class CardComponent {
     @Input()
     public data: ICardData;
 
+    @Input()
+    public type: ICardType;
+
     @Output()
     public clickAction?: EventEmitter<any> = new EventEmitter<any>();
 
-    constructor(public domSanitizer: DomSanitizer) {}
+    constructor(public domSanitizer: DomSanitizer) {
+        this.type = R.contains(this.type, Object.values(ICardType)) ? this.type : null;
+    }
 }
