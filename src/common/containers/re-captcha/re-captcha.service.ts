7bb83d4bed965d7b5008406898be79843bcf80a2
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2021-04-14 13:49:01 +0200

    feature/PXEPR-1341

--- src/common/containers/re-captcha/re-captcha.service.ts
+++ src/common/containers/re-captcha/re-captcha.service.ts
@@ -12,19 +12,19 @@ export class ReCaptchaService {
     public readonly resolveActionSubject$: BehaviorSubject<IResolveAction> = new BehaviorSubject(null);
     public readonly resolveAction$ = this.resolveActionSubject$.asObservable();
 
-    public resolveAction: IResolveAction = null;
+    public executionResolveAction: IResolveAction = null;
     private reCaptcha: RecaptchaComponent = null;
 
     public setReCaptcha = (captchaRef: RecaptchaComponent) => this.reCaptcha = captchaRef;
     public getReCaptcha = (): RecaptchaComponent => this.reCaptcha;
 
     public execute = (resolveAction: IResolveAction) => {
-        this.resolveAction = resolveAction;
+        this.executionResolveAction = resolveAction;
         this.getReCaptcha().execute();
     }
 
     public resolve = (code: string) => {
-        this.resolveActionSubject$.next(this.resolveAction);
+        this.resolveActionSubject$.next(this.executionResolveAction);
         this.reset();
     }
 
