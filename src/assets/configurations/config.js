ae0df7914db016bd38e498c2df54d9e1c171ec3d
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2021-05-19 16:49:27 +0200

    feature/PXEPR-1431

--- src/assets/configurations/config.js
+++ src/assets/configurations/config.js
@@ -19,14 +19,14 @@ window.angularDevstack = {
         "url": "http://localhost:4200",
         "url_graphql": "http://localhost:4200/graphql",
         "url_cms_assets": "https://squidex.lnd.bz/api/assets/",
-        "url_cms": "http://localhost:4200",
+        "url_cms": "https://squidex.lnd.bz",
         "url_cms_local": "https://squidex.lnd.bz",
-        "url_cms_api": "cms/api",
+        "url_cms_api": "api/content/pxe-parc4u/graphql",
         "url_api": "http://localhost:4200/api",
         "web_api_key": "default-api-key",
         "x_api_key": "6DBE159EAAB67C2AFD31DED689608711",
         "includeTestData": true,
-        "useDirectlyCMS": false,
-        "cacheSSR": true
+        "useDirectlyCMS": true,
+        "cacheSSR": false
     }
 };
