5c8d86b6b11e27c9bc4249488a51713d7e06598a
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2021-09-22 19:16:22 +0200

    feature/PXEPR-437

--- src/common/containers/history/overview-container/overview-container.module.ts
+++ src/common/containers/history/overview-container/overview-container.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AlertModule } from 'src/common/ui/alert/alert.module';
 import { ButtonModule } from 'src/common/ui/button/button.module';
+import { InfoBannerModule } from 'src/common/ui/info-banner/info-banner.module';
 import { LogoutInformationModule } from 'src/common/ui/logout-informationation/logout-information.module';
 import { OverviewContainerComponent } from './overview-container.component';
 import { PlaceloaderModule } from 'src/common/ui/placeloader/placeloader.module';
@@ -19,6 +20,7 @@ import { SupplyPointModule } from 'src/common/ui/supply-point/supply-point.modul
         AlertModule,
         ButtonModule,
         CommonModule,
+        InfoBannerModule,
         LogoutInformationModule,
         PlaceloaderModule,
         SupplyPointModule,
