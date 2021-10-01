fcc0a0f14262dca004c3f1c03f6b0ea302d1fbbd
Zdeněk Staněk <zdenek.stanek@lundegaard.eu>
2021-10-01 12:32:34 +0200

    PXEPR-1488-validation-update

--- src/common/constants/errors.constant.ts
+++ src/common/constants/errors.constant.ts
@@ -253,7 +253,7 @@ export const errorFieldMessages =  {
     expirationDate: {
         requiredGas: 'Doplňte datum, dokdy je platná vaše aktuální smlouva na odběr plynu.',
         requiredPower: 'Doplňte datum, dokdy je platná vaše aktuální smlouva na odběr elektřiny.',
-        isInTerminateInterval: 'Vaši aktuální smlouvu už máte automaticky prodlouženou. Vložte správné datum.',
+        isInTerminateInterval: 'V takto krátké době nejsme schopni zajistit přepis smlouvy.',
         expirationAtLeast30DaysBeforeNewDelivery: 'Nejsme schopni v takto kratké době uzavřít navazující smlouvu.',
         notEnoughDaysToProcessContract: 'Smlouva nemůže být změněna, protože nezbývá dostatek času ke zpracování.',
     },
