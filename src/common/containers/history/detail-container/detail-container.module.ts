72dcfd35c8010ab980bb8cfcca07f65ea7ca3f14
tomas.tvrdy <tomas.tvrdy@lundegaard.eu>
2021-11-03 14:01:48 +0100

    feature/PXEPR-1527

--- src/common/containers/history/detail-container/detail-container.module.ts
+++ src/common/containers/history/detail-container/detail-container.module.ts
@@ -2,12 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { AlertModule } from 'src/common/ui/alert/alert.module';
+import { BannerUIModule } from 'src/common/ui/banner/banner-ui.module';
 import { ButtonModule } from 'src/common/ui/button/button.module';
 import { DetailContainerComponent } from './detail-container.component';
 import { FormModule } from 'src/common/ui/forms/form.module';
 import { PlaceloaderModule } from 'src/common/ui/placeloader/placeloader.module';
 import { SupplyPointDetailFormModule } from 'src/common/containers/form/forms/supply-point/detail/supply-point-detail-form.module';
-import {BannerUIModule} from '../../../ui/banner/banner-ui.module';
 
 @NgModule({
     declarations: [
