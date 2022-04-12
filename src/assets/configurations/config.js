376f83ea8853e9ff0d9696d1ce50b6d40e9d1f3b
Zdeněk Staněk <zdenek.stanek@lundegaard.eu>
2022-04-12 23:39:25 +0200

    prerender test confing

--- src/assets/configurations/config.js
+++ src/assets/configurations/config.js
@@ -1,37 +1,27 @@
-// Default variables for all not defined environments and for DEV
-// !!! it is necessary to follow the JSON structure !!!
-//api/content/pxe-parc4u/graphql for localhost developing url_cms_api
-//cms/api for ssr
-//siteKey test all valid
+// Variables for TEST environment
+
 window.angularDevstack = {
     "config": {
         "gaId": "",
+        "environment": "test",
         "gtmId": "GTM-58VS48R",
-        "environment": "preview",
-        "production": false,
+        "production": true,
         "re_captcha": {
             "siteKey": "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI",
             "size": "invisible",
             "errorMode": "handled",
             "badge": "none"
         },
-        "cms": {
-            "client_id": "pxe-parc4u:default",
-            "client_secret": "oummskzkwilyxzzufv1xhcmg7ljxpavxuq6wiu9oizqx",
-            "grand_type": "client_credentials",
-            "scope": "squidex-api"
-        },
-        "sAnalyticsTId": "",
-        "url": "http://localhost:4200",
-        "url_graphql": "http://localhost:4200/graphql",
-        "url_cms_assets": "https://squidex.lnd.bz/api/assets/",
-        "url_cms": "https://squidex.lnd.bz",
-        "url_cms_local": "https://squidex.lnd.bz",
-        "url_cms_api": "api/content/pxe-parc4u/graphql",
-        "url_api": "http://localhost:4200/api",
+        "url": "https://test.parc4u.cz",
+        "url_cms_assets": "https://squidex-preview.lnd.bz/api/assets/pxe-parc4u/",
+        "url_graphql": "https://test.parc4u.cz/graphql",
+        "url_api": "https://test.parc4u.cz/api",
+        "url_cms_api": "cms/api",
+        "url_cms": "https://squidex-preview.lnd.bz",
         "web_api_key": "default-api-key",
         "x_api_key": "6DBE159EAAB67C2AFD31DED689608711",
-        "includeTestData": false,
-        "useDirectlyCMS": true
+        "includeTestData": true,
+        "cacheSSR": false,
+        "useDirectlyCMS": false
     }
 };
